09d0311df7ac94e5e4f13300f3630d14231dc346
Ömer Kayra Akbaş <omakbas@k1m14s06.42istanbul.com.tr>
2025-06-01 18:38:13 +0300

    basit dosya yapısı kurulumu/kayra

--- includes/minishell.h
+++ includes/minishell.h
@@ -3,10 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
+/*   By: omakbas <omakbas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:48:39 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/06/01 16:48:40 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/06/01 18:33:05 by omakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <errno.h>
+
+
+
+#endif
