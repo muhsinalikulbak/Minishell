c80fd7d4c39ae6a11b5850e6b24f41195c8a37ff
Ömer Kayra Akbaş <omakbas@k1m14s06.42istanbul.com.tr>
2025-06-01 17:01:15 +0300

    kayra push/pr deneme

---
+++ main.c
@@ -0,0 +1,12 @@
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <fcntl.h>
+
+
+int main(){
+
+    printf("Kayra psuh pr deneme\n");
+    return 0;
+}
