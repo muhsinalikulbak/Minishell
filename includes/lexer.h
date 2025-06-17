340ae73297543ff07e3504150484105f96b26492
Muhsin <muhsin.kulbak@gmail.com>
2025-06-17 13:02:28 +0300

    lexer.c'de yardımcı fonksiyonlar static yapıldı

--- includes/lexer.h
+++ includes/lexer.h
@@ -6,7 +6,7 @@
 /*   By: muhsin <muhsin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 23:04:23 by muhsin            #+#    #+#             */
-/*   Updated: 2025/06/17 01:52:38 by muhsin           ###   ########.fr       */
+/*   Updated: 2025/06/17 12:58:11 by muhsin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,8 +33,6 @@ typedef struct s_token
 
 
 t_token	*lexer(char *command_line);
-void	scan(char *input_cmd, t_token **token);
-bool	anlayse(char *cmd, t_token **token);
 void	insert_token(t_token **head, char *val, t_token_type token_type);
 void	free_list(t_token **list);
 void	print_list(t_token *list);
--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,68 +6,63 @@
 /*   By: muhsin <muhsin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 18:52:24 by kayraakbas        #+#    #+#             */
-/*   Updated: 2025/06/17 01:52:18 by muhsin           ###   ########.fr       */
+/*   Updated: 2025/06/17 13:01:56 by muhsin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void scan(char *input_cmd, t_token **token)
-{
-	char	**scanned_cmd;
-	int		arg;
-
-	scanned_cmd = ft_split(input_cmd, ' ');
-	if (!scanned_cmd)
-	{
-		perror("command could not scanned");
-		return ;
-	}
-	arg = 0;
-	while (scanned_cmd[arg])
-	{
-		if (anlayse(scanned_cmd[arg], token) == false)
-		{
-			printf("command not found: %s\n",scanned_cmd[arg]);
-			break;
-		}
-		arg++;
-	}
-	arg = 0;
-}
-
-bool anlayse(char *cmd, t_token **token)
+static void	anlayse(char *cmd, t_token **token)
 {
 	int				len;
 	t_token_type	token_type;
-
+	
 	len = ft_strlen(cmd);
 	if (len == 1)
 	{
 		if (cmd[0] == '|')
 		   token_type = TOKEN_PIPE;
 		if (cmd[0] == '<')
-			token_type = TOKEN_REDIR_IN;
+		token_type = TOKEN_REDIR_IN;
 		if (cmd[0] == '>')
-			token_type = TOKEN_REDIR_OUT;
+		token_type = TOKEN_REDIR_OUT;
 	}
 	else if (len == 2)
 	{
 		if (cmd[0] == '<' && cmd[1] == '<')
-			token_type = TOKEN_HEREDOC;
+		token_type = TOKEN_HEREDOC;
 		if (cmd[0] == '>' && cmd[1] == '>')
-			token_type = TOKEN_APPEND;
+		token_type = TOKEN_APPEND;
 	}
 	else
-		token_type = TOKEN_WORD;
+	token_type = TOKEN_WORD;
 	insert_token(token, cmd, token_type);
+}
+
+static bool	scan(char *input_cmd, t_token **token)
+{
+	char	**scanned_cmd;
+	int		i;
+
+	i = 0;
+	scanned_cmd = ft_split(input_cmd, ' ');
+	if (!scanned_cmd)
+	{
+		perror("command could not scanned");
+		return (false);
+	}
+	while (scanned_cmd[i])
+	{
+		anlayse(scanned_cmd[i], token);
+		i++;
+	}
 	return (true);
 }
 
 t_token *lexer(char *command_line)
 {
 	t_token *token;
-
+	
 	token = NULL;
 	scan(command_line, &token);
 	return token;   
