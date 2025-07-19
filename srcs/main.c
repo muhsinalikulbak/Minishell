2236a512f7f9836907875f4cd306342ecd2e94fd
Muhsin <muhsin.kulbak@gmail.com>
2025-07-20 02:14:19 +0300

    signal() çalışması yapıldı

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: muhsin <muhsin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:48:37 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/07/14 15:20:00 by muhsin           ###   ########.fr       */
+/*   Updated: 2025/07/20 02:12:12 by muhsin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,18 @@
 
 extern int value; // lexer.c 'de tanımlı bir global değişken
 
+void	handle_signal(int sig)
+{
+printf("Ctrl+C (SIGINT) yakalandı! Kod: %d\n", sig);
+}
+
 int main (int argc, char **argv, char **env)
 {
-	print(value);
+	signal(SIGINT, handle_signal);
+	// print(value); Lexer.c deki global değişken value'yu yazdırır.
+	while(1)
+	{
+		printf("Çalışıyor\n");
+		sleep(1);
+	}
 }
