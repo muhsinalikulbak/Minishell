47c74c6e7f60983045bdd1226a05ffef32a155bc
Muhsin <muhsin.kulbak@gmail.com>
2025-07-26 01:38:06 +0300

    Errno, error code, perror, is a directory, permission denied çalışmaları yapıldı

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: muhsin <muhsin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:48:37 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/07/20 02:12:12 by muhsin           ###   ########.fr       */
+/*   Updated: 2025/07/26 01:26:54 by muhsin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,20 @@ printf("Ctrl+C (SIGINT) yakalandı! Kod: %d\n", sig);
 
 int main (int argc, char **argv, char **env)
 {
-	signal(SIGINT, handle_signal);
-	// print(value); Lexer.c deki global değişken value'yu yazdırır.
-	while(1)
+	// signal(SIGINT, handle_signal);
+	// // print(value); Lexer.c deki global değişken value'yu yazdırır.
+	// while(1)
+	// {
+	// 	printf("Çalışıyor\n");
+	// 	sleep(1);
+	// }
+	int fd = open("file", O_CREAT | O_RDWR, 0644);
+	if (fd == -1)
 	{
-		printf("Çalışıyor\n");
-		sleep(1);
+		printf("Error code : %d\n", errno);
+		perror("Error");
+		printf("Error : %s\n", strerror(errno));
 	}
+	// EISDIR = is a directory
+	// EACCES = permission denied
 }
